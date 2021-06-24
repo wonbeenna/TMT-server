1391c348d0a4cf281aab923fbe17bed59ce41f89
lee-sangryeol <tkdfuf7600@gmail.com>
2021-06-24 17:08:15 +0900

    fix setting 0.2

--- controller/trip/recommend.ts
+++ controller/trip/recommend.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 
 export const recommend = async (req : Request, res : Response) : Promise<void> => {
     try {
-        res.end();
+        res.send('리커멘드');
     }
     catch (err) {
         res.end();
--- controller/user/myPage.ts
+++ controller/user/myPage.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 
 export const myPage = async (req : Request, res : Response) : Promise<void> => {
     try {
-        res.end();
+        res.send('마이페이지');
     }
     catch (err) {
         res.end();

3becdeaf3c87cdee43df487974459e7347c58acf
lee-sangryeol <tkdfuf7600@gmail.com>
2021-06-25 15:21:27 +0900

    add connect 0.1

--- controller/trip/deleteSpot.ts
+++ controller/trip/deleteSpot.ts
@@ -1,10 +1,12 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
 
-export const deleteSpot = async (req : Request, res : Response) : Promise<void> => {
-    try {
-        res.send('딜리트스팟');
-    }
-    catch (err) {
-        res.end();
-    }
-}
+export const deleteSpot = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    res.send("딜리트스팟");
+  } catch (err) {
+    res.end();
+  }
+};

73dd4bf28c586d93fa10b2e4825140158a2f4387
lee-sangryeol <tkdfuf7600@gmail.com>
2021-07-10 22:05:27 +0900

    add postLike 0.1

--- controller/user/like.ts
+++ controller/user/like.ts
@@ -1,32 +1,27 @@
-
 import userDataModel from "database/userData";
 import { Request, Response } from "express";
+import { verifyAccessToken } from "@tokenController/index";
 
-export const likePost = async (req: Request, res: Response): Promise<void> => {
+export const likePost = async (req: Request, res: Response): Promise<any> => {
   try {
-
-    const { email, place } = req.body;
-    let userData = new userDataModel();
-    userData.email = email;
-    userData.place = place;
-    userData
-      .save()
-      .then((newUserData) => {
-        console.log("Create success");
-        res.status(200).json({
-          message: "Create success",
-          data: {
-            post: newUserData,
-          },
-        });
-      })
-      .catch((err) => {
-        res.status(500).json({
-          message: err,
-        });
-      });
+    const userInfo = await verifyAccessToken(req);
+    if (!userInfo) {
+      res.status(400).send({ message: "access token err" });
+    } else {
+      const { place } = req.body;
+      await userDataModel.update(
+        {
+          email: (<any>userInfo).email,
+        },
+        {
+          $addToSet: { place: place },
+        }
+      );
+      // console.log("place: ", place);
+      res.status(200).json({ place: place });
+    }
   } catch (err) {
-    res.end();
+    res.json(err);
   }
 };
 

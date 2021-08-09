dcaabb8ebd9d7eadf420648cf8bd0619103b90d3
wonbeenna <nwbnwb@naver.com>
2021-08-10 08:23:02 +0900

    pr Test

--- controller/auth/kakao.ts
+++ controller/auth/kakao.ts
@@ -3,6 +3,7 @@ import { Request, Response } from "express";
 export const kakaoLogin = async (req: Request, res: Response): Promise<any> => {
   try {
     return res.send("카카오");
+    // 인가코드 -> 토큰 -> 정보요청
   } catch (err) {
     return err;
   }
